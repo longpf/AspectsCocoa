22edcc08d816dbd7ea37fa9a59af6c8fe0b5cac0
longpengfei <466142249@qq.com>
2017-01-09 15:20:32 +0800

    添加测试数据

--- Example/ViewController.m
+++ Example/ViewController.m
@@ -11,7 +11,7 @@
 
 @interface ViewController ()
 
-
+@property (nonatomic, strong) UILabel *label;
 
 @end
 
@@ -20,6 +20,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.label = [UILabel new];
+    [self.label addObserver:self forSelector:@selector(setText:) withBlock:^(AOPObserverInfo *info,NSString *text){
+        NSLog(@"text = %@",text);
+    }];
+    
+    [self.label addObserver:self forSelector:@selector(drawTextInRect:) withBlock:^(AOPObserverInfo *info,NSValue *rectValue){
+        
+        NSLog(@"rectValue = %@",rectValue);
+        
+    }];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.label.text = @"6666";
+        [self.label drawTextInRect:CGRectZero];
+    });
+    
 }
 
 
